edf42f7857eca607773f20afa732bce40f7fd787
Psychpsyo <psychpsyo@gmail.com>
2022-07-31 10:42:19 +0200

    Fixed github link

--- TouchSecurity.cs
+++ TouchSecurity.cs
@@ -9,10 +9,10 @@ namespace TouchSecurity
         public override string Name => "The Neos Mod That Makes It So That Your Fingers Are No Longer Trusted Touch Sources So You Can Rest Easy At Night Knowing No One Will Shove Buttons Into Your Hand Without Your Permission";
         public override string Author => "Psychpsyo";
         public override string Version => "1.0.0";
-        public override string Link => "https://github.com/Psychpsyo/TheNeosModThatMakesItSoThatYourFingersAreNoLongerTrustedTouchSourcesSoYouCanRestEasyAtNightKnowingNoOneWillShoveButtonsIntoYourHandWithoutYourPermission";
+        public override string Link => "https://github.com/Psychpsyo/TNMTMISTYFANLTTSSYCREANKNOWSBIYHWYP";
 
         public override void OnEngineInit() {
-            Harmony harmony = new Harmony("Psychpsyo.TheNeosModThatMakesItSoThatYourFingersAreNoLongerTrustedTouchSourcesSoYouCanRestEasyAtNightKnowingNoOneWillShoveButtonsIntoYourHandWithoutYourPermission");
+            Harmony harmony = new Harmony("Psychpsyo.TNMTMISTYFANLTTSSYCREANKNOWSBIYHWYP");
             harmony.PatchAll();
         }
 
