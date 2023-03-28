cb9d139103c51b224e028d8bf9b9f04a10b07027
Psychpsyo <psychpsyo@gmail.com>
2023-03-28 23:36:01 +0200

    Fix version (1.0.1 -> 1.1.0)

--- TouchSecurity.cs
+++ TouchSecurity.cs
@@ -8,7 +8,7 @@ namespace TouchSecurity
     {
         public override string Name => "The Neos Mod That Makes It So That Your Fingers Are No Longer Trusted Touch Sources So You Can Rest Easy At Night Knowing No One Will Shove Buttons Into Your Hand Without Your Permission";
         public override string Author => "Psychpsyo";
-        public override string Version => "1.0.1";
+        public override string Version => "1.1.0";
         public override string Link => "https://github.com/Psychpsyo/TNMTMISTYFANLTTSSYCREANKNOWSBIYHWYP";
 
         public static ModConfiguration Config;
